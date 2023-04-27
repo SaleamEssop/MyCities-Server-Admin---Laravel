81e4b7047351af7a28575f3afdfa5347b43050fc
“Mitesh <mitesh0267@gmail.com>
2023-04-27 22:13:15 +0530

    issue fixed

--- resources/views/admin/region_cost/index.blade.php
+++ resources/views/admin/region_cost/index.blade.php
@@ -2,26 +2,26 @@
 @section('title', 'Regions')
 
 @section('content')
-    <!-- Begin Page Content -->
-    <div class="container-fluid">
+<!-- Begin Page Content -->
+<div class="container-fluid">
 
-        <!-- Page Heading -->
-        <div class="cust-page-head">
-            <h1 class="h3 mb-2 custom-text-heading">Regions Cost</h1>
-            <!-- <button type="button" class="btn btn-warning btn-circle" data-toggle="modal" data-target="#costModal">
+    <!-- Page Heading -->
+    <div class="cust-page-head">
+        <h1 class="h3 mb-2 custom-text-heading">Regions Cost</h1>
+        <!-- <button type="button" class="btn btn-warning btn-circle" data-toggle="modal" data-target="#costModal">
                 <i class="fas fa-plus-square"></i>
             </button> -->
-        </div>
+    </div>
 
-        <!-- DataTales Example -->
-        <div class="card shadow mb-4">
-            <div class="card-header py-3">
-                <h6 class="m-0 font-weight-bold">List of Regions/AccountType Cost added by the admin</h6>
-            </div>
-            <div class="card-body">
-                <div class="table-responsive">
-                    <table class="table table-bordered" id="sites-dataTable" width="100%" cellspacing="0">
-                        <thead>
+    <!-- DataTales Example -->
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold">List of Regions/AccountType Cost added by the admin</h6>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered" id="sites-dataTable" width="100%" cellspacing="0">
+                    <thead>
                         <tr>
                             <th>#</th>
                             <th>Template Name</th>
@@ -30,63 +30,76 @@
                             <th>Created Date</th>
                             <th>Actions</th>
                         </tr>
-                        </thead>
-                        <tfoot>
+                    </thead>
+                    <tfoot>
                         <tr>
                             <th>#</th>
                             <th>Template Name</th>
                             <th>Region</th>
                             <th>AccountType</th>
-                            
+
                             <th>Created Date</th>
                             <th>Actions</th>
                         </tr>
-                        </tfoot>
-                        <tbody>
+                    </tfoot>
+                    <tbody>
                         @foreach($regionsAccountTypeCost as $cost)
-                            <tr>
-                                <td>{{ $loop->iteration }}</td>
-                                <td>{{ $cost->template_name }}</td>
-                                <td>{{ $cost->region->name }}</td>
-                                <td>{{ $cost->accountType->type }}</td>
-                                <td>{{ $cost->created_at }}</td>
-                                <td>
-                                    <a href="{{ url('admin/region_cost/edit/'.$cost->id) }}" class="btn btn-warning btn-circle">
-                                        <i class="fas fa-edit"></i>
-                                    </a>
-                                    <a href="{{ url('admin/region_cost/delete/'.$cost->id) }}" onclick="return confirm('Are you sure you want to delete this region cost?')" class="btn btn-danger btn-circle">
-                                        <i class="fas fa-trash"></i>
-                                    </a>
-                                </td>
-                            </tr>
+                        <tr>
+                            <td>{{ $loop->iteration }}</td>
+                            @if(isset($cost->template_name))
+                            <td>{{ $cost->template_name }}</td>
+                            @else
+                            <td></td>
+                            @endif
+                            @if(isset($cost->region))
+                            <td>{{ $cost->region->name }}</td>
+                            @else
+                            <td></td>
+                            @endif
+                            @if(isset($cost->accountType))
+                            <td>{{ $cost->accountType->type }}</td>
+                            @else
+                            <td></td>
+                            @endif
+
+                            <td>{{ $cost->created_at }}</td>
+                            <td>
+                                <a href="{{ url('admin/region_cost/edit/'.$cost->id) }}" class="btn btn-warning btn-circle">
+                                    <i class="fas fa-edit"></i>
+                                </a>
+                                <a href="{{ url('admin/region_cost/delete/'.$cost->id) }}" onclick="return confirm('Are you sure you want to delete this region cost?')" class="btn btn-danger btn-circle">
+                                    <i class="fas fa-trash"></i>
+                                </a>
+                            </td>
+                        </tr>
                         @endforeach
-                        </tbody>
-                    </table>
-                </div>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
+</div>
 
-    <!-- Modal -->
-    <div class="modal fade" id="costModal" tabindex="-1" role="dialog" aria-labelledby="costModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="costModalLabel">Add New Region Cost</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-
+<!-- Modal -->
+<div class="modal fade" id="costModal" tabindex="-1" role="dialog" aria-labelledby="costModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="costModalLabel">Add New Region Cost</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
             </div>
+
         </div>
     </div>
+</div>
 @endsection
 
 @section('page-level-scripts')
-    <script type="text/javascript">
-        $(document).ready(function() {
-            $('#sites-dataTable').dataTable();
-        });
-    </script>
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('#sites-dataTable').dataTable();
+    });
+</script>
 @endsection
