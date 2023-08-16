368d8441ddcf59f6e6879b4eeaad050214698000
Kamran <kamranallahyar@outlook.com>
2023-08-17 00:11:15 +0500

    meta tags added

--- resources/views/landing_page.blade.php
+++ resources/views/landing_page.blade.php
@@ -9,7 +9,7 @@
     <meta property="og:site_name" content="The MyCities App">
     <meta property="og:title" content="The MyCities App">
     <meta property="og:description" content="Helping the cities people save with app technology, ideas, determination and cooperation.">
-    <meta property="og:image" itemprop="image" content="{{url('/img/my_cities.png',[],true)}}">
+    <meta property="og:image" content="{{url('/img/my_cities.png',[],true)}}">
     <meta itemprop="image" content="{{url('/img/my_cities.png',[],true)}}">
     <meta property="og:image:width" content="600">
     <meta property="og:image:height" content="600">
