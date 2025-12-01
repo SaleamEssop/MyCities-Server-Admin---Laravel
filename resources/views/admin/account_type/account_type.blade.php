e933ed7e5d1caa900bfcc40566e9b816b571c84e
Saleam Essop <34599175+SaleamEssop@users.noreply.github.com>
2025-12-01 12:11:05 +0200

    Rename 'Account Type' to 'Tariff Categories'

--- resources/views/admin/account_type/account_type.blade.php
+++ resources/views/admin/account_type/account_type.blade.php
@@ -1,5 +1,5 @@
 @extends('admin.layouts.main')
-@section('title', 'Regions')
+@section('title', 'Tariff Categories')
 
 @section('content')
     <!-- Begin Page Content -->
@@ -7,16 +7,13 @@
 
         <!-- Page Heading -->
         <div class="cust-page-head">
-            <h1 class="h3 mb-2 custom-text-heading">Account Type</h1>
-            <!-- <button type="button" class="btn btn-warning btn-circle" data-toggle="modal" data-target="#costModal">
-                <i class="fas fa-plus-square"></i>
-            </button> -->
+            <h1 class="h3 mb-2 custom-text-heading">Tariff Categories</h1>
         </div>
 
         <!-- DataTales Example -->
         <div class="card shadow mb-4">
             <div class="card-header py-3">
-                <h6 class="m-0 font-weight-bold">List of Account Type added by the admin</h6>
+                <h6 class="m-0 font-weight-bold">List of Tariff Categories added by the admin</h6>
             </div>
             <div class="card-body">
                 <div class="table-responsive">
@@ -25,10 +22,6 @@
                         <tr>
                             <th>#</th>
                             <th>Name</th>
-                            <!-- <th>Water Base Unit</th>
-                            <th>Water Base Unit Cost</th>
-                            <th>Electricity Base Unit</th>
-                            <th>Electricity Base Unit Cost</th> -->
                             <th>Created Date</th>
                             <th>Actions</th>
                         </tr>
@@ -37,10 +30,6 @@
                         <tr>
                             <th>#</th>
                             <th>Name</th>
-                            <!-- <th>Water Base Unit</th>
-                            <th>Water Base Unit Cost</th>
-                            <th>Electricity Base Unit</th>
-                            <th>Electricity Base Unit Cost</th> -->
                             <th>Created Date</th>
                             <th>Actions</th>
                         </tr>
@@ -55,7 +44,7 @@
                                     <a href="{{ url('admin/account_type/edit/'.$accounttype->id) }}" class="btn btn-warning btn-circle">
                                         <i class="fas fa-edit"></i>
                                     </a>
-                                    <a href="{{ url('admin/account_type/delete/'.$accounttype->id) }}" onclick="return confirm('Are you sure you want to delete this account type?')" class="btn btn-danger btn-circle">
+                                    <a href="{{ url('admin/account_type/delete/'.$accounttype->id) }}" onclick="return confirm('Are you sure you want to delete this tariff category?')" class="btn btn-danger btn-circle">
                                         <i class="fas fa-trash"></i>
                                     </a>
                                 </td>
@@ -64,21 +53,6 @@
                         </tbody>
                     </table>
                 </div>
-            </div>
-        </div>
-    </div>
-
-    <!-- Modal -->
-    <div class="modal fade" id="costModal" tabindex="-1" role="dialog" aria-labelledby="costModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="costModalLabel">Add New Account Type</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-
             </div>
         </div>
     </div>
